6048b8bacf5e789a7c0b3eac2d41308ea4325e1f
Jacky Tang <tangyingzong@gmail.com>
2017-08-03 16:28:51 +0800

    init

---
+++ js/genvoucher.js
@@ -0,0 +1,25 @@
+﻿
+$("#genvoucher").click(function (event) {
+    var param = {
+        "Phone": $("#telno").text(),
+        "PIN_Code": $("#pincode").val()
+}
+    $.ajax({
+        "url": "https://www.billionvouchers.com/BVwebapi/" + "BVSP_VOUCHER_GENERATE",
+        "async": true,
+        "crossDomain": true,
+        "type": "POST",
+        "dataType": "json",
+        "contentType": "application/json; charset=utf-8",
+        "data": JSON.stringify(param),
+        "success": function (result) {
+            if (result.ResultSets[0].length > 0)
+                {
+                console.log('激活失败：' + result.ResultSets[0][0].msg);
+                $("#msg").html( = '激活失败：' + result.ResultSets[0][0].msg);
+            }
+            else
+                console.log('激活成功');
+        }
+    })
+});
